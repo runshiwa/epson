7ba5792ef5e3e35b051ee699a30f7a5cf636b7b9
Nobuyuki Shimazu <nobuyuki@a.email.ne.jp>
2024-04-20 04:19:25 +0900

    parameterize line threshold

--- common.py
+++ common.py
@@ -8,4 +8,5 @@ EPSON_STRING_GUARD_CS = string.ascii_letters + string.digits
 EPSON_STRING_GUARD_LEN = 9
 
 JSON_STRING = re.compile(r'"((?:\\.|[^"\\])*)"')
+JSON_STRING_LINE_THRESHOLD = 1
 JSON_DUMP_KWARG = dict(ensure_ascii=False, indent="\t", default=repr)
