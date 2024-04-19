5a5ede5e7fb84729a341f7bd7537b8ec578df9c3
runshiwa <nobuyuki@a.email.ne.jp>
2024-04-20 03:15:14 +0900

    first commit

---
+++ common.py
@@ -0,0 +1,11 @@
+import re
+import string
+
+EPSON_STRING = re.compile(r'""(.+)\n(.*\n)*?\1\n')
+EPSON_STRING_START = '""'
+EPSON_STRING_GUARD = "."
+EPSON_STRING_GUARD_CS = string.ascii_letters + string.digits
+EPSON_STRING_GUARD_LEN = 9
+
+JSON_STRING = re.compile(r'"((?:\\.|[^"\\])*)"')
+JSON_DUMP_KWARG = dict(ensure_ascii=False, indent="\t", default=repr)
