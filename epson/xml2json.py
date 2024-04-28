75dea2aed66eb2e327a7ac7b92e745867af7e999
runshiwa <nobuyuki@a.email.ne.jp>
2024-04-28 20:42:50 +0900

    adjust variable name

--- epson/xml2json.py
+++ epson/xml2json.py
@@ -53,9 +53,9 @@ def simplify_object(obj):
             if item
         }
         if len(obj) == 1:
-            return obj.popitem()[1]
+            obj =  obj.popitem()[1]
         elif len(obj) == 0:
-            return None
+            obj = None
     elif isinstance(obj, list):
         obj = [
             item
@@ -63,85 +63,85 @@ def simplify_object(obj):
             if item
         ]
         if len(obj) == 1:
-            return obj[0]
+            obj = obj[0]
         elif len(obj) == 0:
-            return None
+            obj = None
     return obj
 
 
 def etdom2object(element, strip=True, simplify=True, simplify2=False):
     # element is object with tag name
-    o = {}
+    element_object = {}
     # element has attributes, text, child(ren), tail as list
-    o[element.tag] = {}
+    element_object[element.tag] = {}
 
     # append element attributes
-    o[element.tag][ATTRIBUTE] = {}
+    element_object[element.tag][ATTRIBUTE] = {}
     for attribute in element.attrib:
-        o[element.tag][ATTRIBUTE][attribute] = element.attrib[attribute]
+        element_object[element.tag][ATTRIBUTE][attribute] = element.attrib[attribute]
 
     # append element text
-    o[element.tag][TEXT] = element.text
+    element_object[element.tag][TEXT] = element.text
 
     # append element child(ren)
-    o[element.tag][CHILDREN] = []
+    element_object[element.tag][CHILDREN] = []
     for child in element:
-        o[element.tag][CHILDREN] += [
+        element_object[element.tag][CHILDREN] += [
             etdom2object(child, strip, simplify, simplify2)]
 
     # append element tail
-    o[element.tag][TAIL] = element.tail
+    element_object[element.tag][TAIL] = element.tail
 
     if strip:
-        if o[element.tag][TEXT] is not None:
-            o[element.tag][TEXT] = o[element.tag][TEXT].strip()
-            if len(o[element.tag][TEXT]) == 0:
-                o[element.tag][TEXT] = None
-        if o[element.tag][TAIL] is not None:
-            o[element.tag][TAIL] = o[element.tag][TAIL].strip()
-            if len(o[element.tag][TAIL]) == 0:
-                o[element.tag][TAIL] = None
+        if element_object[element.tag][TEXT] is not None:
+            element_object[element.tag][TEXT] = element_object[element.tag][TEXT].strip()
+            if len(element_object[element.tag][TEXT]) == 0:
+                element_object[element.tag][TEXT] = None
+        if element_object[element.tag][TAIL] is not None:
+            element_object[element.tag][TAIL] = element_object[element.tag][TAIL].strip()
+            if len(element_object[element.tag][TAIL]) == 0:
+                element_object[element.tag][TAIL] = None
     if simplify:
-        o[element.tag] = simplify_etelement(o[element.tag])
+        element_object[element.tag] = simplify_etelement(element_object[element.tag])
     if simplify2:
-        o[element.tag] = simplify_object(o[element.tag])
+        element_object[element.tag] = simplify_object(element_object[element.tag])
 
-    return o
+    return element_object
 
 
 def dom2object(element, strip=True, simplify=True, simplify2=False):
     # element is object with tag name
-    o = {}
+    element_object = {}
     # element has attributes, text, child(ren)
-    o[element.tagName] = {}
+    element_object[element.tagName] = {}
 
     # append element attributes
-    o[element.tagName][ATTRIBUTE] = {}
+    element_object[element.tagName][ATTRIBUTE] = {}
     for attribute, value in element.attributes.items():
-        o[element.tagName][ATTRIBUTE][attribute] = value
+        element_object[element.tagName][ATTRIBUTE][attribute] = value
 
     # append element text
-    o[element.tagName][TEXT] = "".join(
+    element_object[element.tagName][TEXT] = "".join(
         child.data for child in element.childNodes if child.nodeType == xml.dom.Node.TEXT_NODE)
 
     # append element child(ren)
-    o[element.tagName][CHILDREN] = []
+    element_object[element.tagName][CHILDREN] = []
     for child in element.childNodes:
         if child.nodeType == xml.dom.Node.ELEMENT_NODE:
-            o[element.tagName][CHILDREN] += [
+            element_object[element.tagName][CHILDREN] += [
                 dom2object(child, strip, simplify, simplify2)]
 
     if strip:
-        if o[element.tagName][TEXT] is not None:
-            o[element.tagName][TEXT] = o[element.tagName][TEXT].strip()
-            if len(o[element.tagName][TEXT]) == 0:
-                o[element.tagName][TEXT] = None
+        if element_object[element.tagName][TEXT] is not None:
+            element_object[element.tagName][TEXT] = element_object[element.tagName][TEXT].strip()
+            if len(element_object[element.tagName][TEXT]) == 0:
+                element_object[element.tagName][TEXT] = None
     if simplify:
-        o[element.tagName] = simplify_element(o[element.tagName])
+        element_object[element.tagName] = simplify_element(element_object[element.tagName])
     if simplify2:
-        o[element.tagName] = simplify_object(o[element.tagName])
+        element_object[element.tagName] = simplify_object(element_object[element.tagName])
 
-    return o
+    return element_object
 
 
 def object2json(o, *arg, **kwarg):
